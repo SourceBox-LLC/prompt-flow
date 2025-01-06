9df6372d109d59af7e047cddfb19c91a217397c2
sbussiso <sbussiso321@gmail.com>
2025-01-05 21:42:26 -0800

    made dummy combine and split blocks for combining and connecting multiple block nodes

--- app.py
+++ app.py
@@ -175,6 +175,55 @@ def parser_block_compute(self):
     else:
         self.set_interface(name='double_quote', value=None)
 
+def pack_block_compute(self):
+    """
+    Compute function for the Pack Block.
+    """
+    in_val = self.get_interface(name='input_0')
+    if in_val:
+        st.sidebar.write("Pack Block received input:", in_val)
+        # Dummy processing logic
+        out_val = f"Packed: {in_val}"
+        self.set_interface(name='output_0', value=out_val)
+        st.sidebar.write("Pack Block set output:", out_val)
+    else:
+        st.sidebar.write("Pack Block received no input.")
+
+def split_block_compute(self):
+    """
+    Compute function for the Split Block.
+    """
+    in_val = self.get_interface(name='input_0')
+    if in_val:
+        st.sidebar.write("Split Block received input:", in_val)
+        # Dummy processing logic to split input into three parts
+        part1 = f"Part 1: {in_val}"
+        part2 = f"Part 2: {in_val}"
+        part3 = f"Part 3: {in_val}"
+        self.set_interface(name='output_1', value=part1)
+        self.set_interface(name='output_2', value=part2)
+        self.set_interface(name='output_3', value=part3)
+        st.sidebar.write("Split Block set outputs:", part1, part2, part3)
+    else:
+        st.sidebar.write("Split Block received no input.")
+
+def combine_block_compute(self):
+    """
+    Compute function for the Combine Block.
+    """
+    in_val1 = self.get_interface(name='input_1')
+    in_val2 = self.get_interface(name='input_2')
+    in_val3 = self.get_interface(name='input_3')
+    
+    if in_val1 and in_val2 and in_val3:
+        st.sidebar.write("Combine Block received inputs:", in_val1, in_val2, in_val3)
+        # Dummy processing logic to combine inputs
+        combined_val = f"Combined: {in_val1}, {in_val2}, {in_val3}"
+        self.set_interface(name='output_0', value=combined_val)
+        st.sidebar.write("Combine Block set output:", combined_val)
+    else:
+        st.sidebar.write("Combine Block received incomplete inputs.")
+
 ###############################################################################
 # 2. Utility Functions
 ###############################################################################
@@ -279,6 +328,25 @@ def main_page():
     wikipedia_block.add_output(name='output_0')
     wikipedia_block.add_compute(wikipedia_search_compute)
 
+    pack_block = Block(name='Pack Block')
+    pack_block.add_input(name='input_0')
+    pack_block.add_output(name='output_0')
+    pack_block.add_compute(pack_block_compute)
+
+    split_block = Block(name='Split Block')
+    split_block.add_input(name='input_0')
+    split_block.add_output(name='output_1')
+    split_block.add_output(name='output_2')
+    split_block.add_output(name='output_3')
+    split_block.add_compute(split_block_compute)
+
+    combine_block = Block(name='Combine Block')
+    combine_block.add_input(name='input_1')
+    combine_block.add_input(name='input_2')
+    combine_block.add_input(name='input_3')
+    combine_block.add_output(name='output_0')
+    combine_block.add_compute(combine_block_compute)
+
     # ------------------------------------------------
     # Create a Prompt block PER template in session_state
     # ------------------------------------------------
@@ -316,6 +384,9 @@ def main_page():
             pubmed_block,
             wikipedia_block,
             final_output,
+            pack_block,
+            split_block,
+            combine_block,
             *all_prompt_blocks  # add all generated prompt blocks
         ],
         key=st.session_state["barfi_key"]
